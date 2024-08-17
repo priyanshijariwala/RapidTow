072726beb9b0d8cc089b572f9f00ae3551d422eb
priyanshijariwala <priyanshi1jariwala@gmail.com>
2024-08-17 15:33:05 +0530

    vehicle_module

---
+++ models/vehicle.js
@@ -0,0 +1,41 @@
+
+const mongoose=require("mongoose");
+const {Schema}=mongoose;
+
+const vehicleSchema=new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user',
+    },
+    vehicle_model_name:{
+        type:String,
+        require:true,
+    },
+    vehicle_company_name:{
+        type:String,
+        require:true,
+    },
+    vehicle_number:{
+        type:Number,
+        require:true,
+    },
+    
+    vehicle_model_name:{
+        type:String,
+        require:true,
+    },
+    old_destination:{
+        type:String,
+        require:true,
+    },
+    new_destination:{
+        type:String,
+        require:true,
+    },
+    payment_mode:{
+        type:String,
+        require:true,
+    }
+});
+const vehicle = mongoose.model('registration', vehicleSchema);
+module.exports = vehicle;
