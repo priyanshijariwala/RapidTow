30f0457aa0a4aa7a5fece41ca1d357a90bcf2dab
priyanshijariwala <priyanshi1jariwala@gmail.com>
2024-08-30 16:21:55 +0530

    home update

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -8,13 +8,14 @@ import '../style.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 // import { useState } from 'react';
 
 
 
 
 function Home() {
-  
+
 
   return (
     <>
@@ -44,50 +45,87 @@ function Home() {
 
 
         </div>
-        <div className="line"></div>
       </div>
+      <div id='top-services'>
+        <h4>Top Services</h4>
+        <div className='d-flex align-items-center justify-content-center'>
+          <Row className='row '>
+            <Col className='col'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="/image/rescuetruck_1.jpg" />
+                <Card.Body>
+                  <Card.Title>Rescue Rangers</Card.Title>
+                  <Card.Text>
+                    Top rescue team for rescue.
+                  </Card.Text>
+                  <Button variant="primary">Book</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col className='col'>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="../image/rescuetruck_2.jpg" />
+                <Card.Body>
+                  <Card.Title>Emergency Tow</Card.Title>
+                  <Card.Text>
+                    Provide fastest Tow
+                  </Card.Text>
+                  <Button variant="primary">Book</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+            
+          </Row>
 
-      <div>
-        <Row className='row text-center'>
-          <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="/image/rescuetruck_1.jpg" />
-              <Card.Body>
-                <Card.Title>Rescue Rangers</Card.Title>
-                <Card.Text>
-                  Top rescue team for rescue.
-                </Card.Text>
-                <Button variant="primary">Book</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="../image/rescuetruck_2.jpg" />
-              <Card.Body>
-                <Card.Title>Emergency Tow</Card.Title>
-                <Card.Text>
-                  Provide fastest Tow
-                </Card.Text>
-                <Button variant="primary">Book</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          {/* <Col>
-            <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the
-                  bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col> */}
-        </Row>
-
+        </div>
+      </div>
+      <div id='local-provider'>
+        <h4>Local Providers</h4>
+        <Row>
+        <Col xs={4} md={3}>
+          <Image src="../image/rescuetruck_3.jpg" rounded />
+          <h6>Swift Tow</h6>
+          <p>Gear Garrage.</p>
+        </Col>
+        <Col xs={4} md={3}>
+          <Image src="./image/rescuetruck_4.jpg" roundedCircle />
+          <h6>Road Rescuers</h6>
+          <p>Quick Fix Center. </p>
+        </Col>
+        <Col xs={4} md={3}>
+          <Image src="/image/rescuetruck_3.jpg" thumbnail />
+          <h6>Tow Titans</h6>
+          <p>Auto Depot.</p>
+        </Col>
+        <Col xs={4} md={3}>
+          <Image src="image/rescuetruck_4.jpg" thumbnail />
+          <h6>Rescue Me</h6>
+          <p>Tow Zone.</p>
+        </Col>
+      </Row>
+      </div>
+      <div id='services'>
+        <h4>Services</h4>
+        <Row>
+        <Col xs={3} md={2}>
+          <p className='service'>Tow</p>
+        </Col>
+        <Col xs={3} md={2}>
+          <p className='service'>Assist</p>
+        </Col>
+        <Col xs={3} md={2}>
+          <p className='service'>Help</p>
+        </Col>
+        <Col xs={3} md={2}>
+          <p className='service'>Rescue</p>
+        </Col>
+        <Col xs={3} md={2}>
+          <p className='service'>Support</p>
+        </Col>
+        <Col xs={3} md={2}>
+          <p className='service'>6</p>
+        </Col>
+      </Row>
       </div>
     </>
   )
