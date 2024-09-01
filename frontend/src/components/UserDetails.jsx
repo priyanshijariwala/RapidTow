f81f05115b5df3563b456c3ba375333572800e23
priyanshijariwala <priyanshi1jariwala@gmail.com>
2024-09-01 16:15:16 +0530

    userdetails & vehicle details

--- frontend/src/components/UserDetails.jsx
+++ frontend/src/components/UserDetails.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
 
 function UserDetails() {
   return (
-    <div>UserDetails</div>
+    <>
+    <div>
+    <h4>UserDetails</h4>
+    
+    </div>
+    <div>
+          <Row>
+            <Col sm={6}>
+              <Form.Control type="text" placeholder="Username" />
+            </Col>
+            <Col sm={6}>
+              <Form.Control type="email" placeholder="Email" />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Fullname" />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Contact Number" />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="date" placeholder="DOB" />
+            </Col>
+          </Row>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 1 }}>
+              <Button type="submit">Edit</Button>
+            </Col>
+          </Form.Group>
+    </div>
+    </>
   )
 }
 
--- frontend/src/components/VehicleDetails.jsx
+++ frontend/src/components/VehicleDetails.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
 
 function VehicleDetails() {
   return (
-    <div>VehicleDetails</div>
+    <>
+    <div>
+    <h4>VehicleDetails</h4>
+    
+    </div>
+    <div>
+          <Row>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Vehicle_model_name" />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Vehicle_company_name" />
+            </Col>
+            <Col sm={4}>
+              <Form.Control type="text" placeholder="Vehicle_number" />
+            </Col>
+          </Row>
+
+          <Form.Group as={Row} className="mb-3">
+            <Col sm={{ span: 10, offset: 1 }}>
+              <Button type="submit">Edit</Button>
+            </Col>
+          </Form.Group>
+    </div>
+    </>
   )
 }
 
